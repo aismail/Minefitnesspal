fb3dd49398422aae18ffbda735a620621f3f89c1
Christopher Buja <csbuja@umich.edu>
2015-01-09 14:38:28 -0800

    before pipelining entire process

--- createCSVData.py
+++ createCSVData.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sklearn.cluster
 from pprint import pprint
 import os
+import sys
 from makeArrayFromPrint import makearrayfromprint
 json_data=open('data.txt')
 import string
@@ -69,14 +70,15 @@ for key in data:
             csvData[word][i]+=1
     i+=1
 y = csvData.ix[:,len(csvData.columns)-1].as_matrix()
-data = csvData.ix[:,:len(csvData.columns)-1].as_matrix()
+X = csvData.ix[:,:len(csvData.columns)-1].as_matrix()
 
 MS = sklearn.cluster.AffinityPropagation()
-x = MS.fit_predict(data)
-# results from MS 
+x = MS.fit_predict(X)
+# results from MS -- only used if clustering algorithm takes a long time
 # c = makearrayfromprint("[ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 12 0 0 0 0 0 0 0 0 0 0 0 0 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 0 0 0 0 7 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 14 0 0 0 0 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 15 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3 0 0 0 0 0 0 0 0 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]")
 c = x
 
+sub_dir = "output" #where all output files will go
 clusters ={}
 for i,val in enumerate(c):
     if val == 0:
@@ -87,19 +89,48 @@ for i,val in enumerate(c):
         currAve = clusters[val][0]
         newAve = (currAve*float(len(clusters[val])-1) + y[i])/float(len(clusters[val]))
         clusters[val].append(messages[i])
-
-for key in clusters:
-    print "Score for cluster " + str(key) + ": " +str(clusters[key][0])
-    clusters[key] =clusters[key][1:]
+filename = "clusterInfo.txt"
+try:
+        os.remove(os.path.join(sub_dir, filename))
+except OSError:
+    pass
+with open(os.path.join(sub_dir, filename), 'w+') as myfile:
+    for key in clusters:
+        s = "Cluster " + str(key) + ":\n   Average Shares: " +str(clusters[key][0]) + "\n   Size: " + str(len(clusters[key])-1)
+        myfile.write(s + "\n")
+        clusters[key] =clusters[key][1:]
 
 for key in clusters:
     filename = str(key) + ".txt"
     try:
-        os.remove(filename)
+        os.remove(os.path.join(sub_dir, filename))
     except OSError:
         pass
-    with open(filename, 'w+') as myfile:
+    with open(os.path.join(sub_dir, filename), 'w+') as myfile:
         for s in clusters[key]:
             myfile.write(filter(lambda x: x in string.printable, s) + "\n")
+
+filename = ""
+if len(sys.argv)>1:
+    filename = sys.argv[1]
+else:
+    filename = "output.csv"
+try:
+    os.remove(os.path.join(sub_dir, filename))
+except OSError:
+    pass
+with open(os.path.join(sub_dir, filename), 'w+') as myfile:
+    myfile.write("postId,clusterId,message" + "\n")
+    i = 0
+    for key in data:
+        s = data[key]['message']
+        toWrite = str(key) + "," 
+        toWrite += str(c[i]) + "," 
+        toWrite += '"' + filter(lambda x: x in string.printable, s) +'"'
+        myfile.write(toWrite + "\n")
+        i+=1
+    
+
+
 
 
--- getData/getthemposts.js
+++ getData/getthemposts.js
@@ -1,6 +1,7 @@
 var request = require("request");
 var _ = require("underscore");
 var FB = require('fb');
+var fs = require('fs');
 
 FB.setAccessToken('1543530219228022|l2ghob0T2nzJOYeiCvm2XlX--ws');
 
@@ -19,16 +20,20 @@ var NUM_CHARS_UNTIL_NEXT_PAGE_ROUTE = "https://graph.facebook.com/v2.2".length;
 var getPosts = function(posts,url,depth){
     console.log("depth: " + depth);
     if (Object.keys(posts).length>500) {
-        console.log(JSON.stringify(posts));
-        return posts;
+        console.log();
+        var filename = "../data.txt"
+        // fs.writeFile(filename,JSON.stringify(posts), function (err) {
+        //   if (err) throw err;
+        //   console.log('It\'s written in data.txt in the previous directory!');
+        // });
+        return JSON.stringify(posts);
     }
     
     request.get(url, function (req,res) {
         res = JSON.parse(res.body);
         if(!res || res.error) {
             console.log(!res ? 'error occurred' : res.error);
-            console.log(posts)
-            return;
+            return JSON.stringify(posts);
         }
         _.each(res.data,function(v){
             if (!v.likes || !v.comments || !v.message || !v.shares) {
@@ -42,7 +47,7 @@ var getPosts = function(posts,url,depth){
             posts[v.id] = post;
         });
         url = res.paging.next;
-        getPosts(posts,url,depth+1);
+        return getPosts(posts,url,depth+1);
     });
 }
 var x = getPosts(posts,url,0)
